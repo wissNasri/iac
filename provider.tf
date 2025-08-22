15c017dab703877664d4d40e36b3bf1815a314ba
wissNasri <nasri.wissen@ieee.org>
2025-08-22 10:17:31 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+locals {
+  region          = "eu-north-1"
+  name            = "tws-eks-cluster"
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["eu-north-1a", "eu-north-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  tags = {
+    example = local.name
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
