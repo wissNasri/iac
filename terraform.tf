cbbcb380540bd24725f194977394e71e05cb47b3
wissNasri <nasri.wissen@ieee.org>
2025-08-22 10:32:12 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-s3-backend-tws-hackathon"
+    bucket         = "terraform-s3-backend-tws-hackathon11"
     key            = "backend-locking"
     region         = "eu-north-1"       # Valeur fixe, local.region ne fonctionne pas ici
     dynamodb_table = "terraform-locks"
