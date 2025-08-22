c86c04213e8bf1763a00b92e8acfefeff283d66e
wissNasri <nasri.wissen@ieee.org>
2025-08-22 10:18:31 +0100

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.18.1"
+
+  name            = local.name
+  cidr            = local.vpc_cidr
+  azs             = local.azs
+  public_subnets  = local.public_subnets
+  private_subnets = local.private_subnets
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  map_public_ip_on_launch = true
+}
