4dad7ee9528327d4e69808b75292f30eebc91eb5
wissNasri <nasri.wissen@ieee.org>
2025-09-14 14:35:15 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,31 @@
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
+output "region" {
+  description = "The AWS region where resources are created"
+  value       = local.region
 }
 
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+
 output "eks_cluster_name" {
-  value = module.eks.cluster_name
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
 output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "EKS cluster API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.bastion_host.public_ip
 }
 
 output "eks_node_group_public_ips" {
-  value = module.eks.eks_managed_node_groups["ng1"].instances_public_ips
+  description = "Public IPs of the EKS node group instances"
+  value       = data.aws_instances.eks_nodes.public_ips
 }
