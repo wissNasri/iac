26cf82af78a77320df11cf52f429f8d8581da139
wissNasri <nasri.wissen@ieee.org>
2025-09-14 14:32:51 +0100

    Update and rename bastion_user_data.sh to bastion_user_data.sh

--- terraform/bastion_user_data.sh
+++ terraform/bastion_user_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Mettre à jour les paquets et installer les dépendances de base + Git
 sudo apt-get update -y
-sudo apt-get install snapd -y
+sudo apt-get install -y snapd gnupg software-properties-common curl git
 
 # Install AWS CLI
 sudo snap install aws-cli --classic
@@ -10,3 +11,8 @@ sudo snap install helm --classic
 
 # Install Kubectl
 sudo snap install kubectl --classic
+# Install Terraform (official method)
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update -y
+sudo apt-get install -y terraform
