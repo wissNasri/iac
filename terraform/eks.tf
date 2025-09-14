e69d3ad61332192f7f206c041dd7ba0da71e1289
wissNasri <nasri.wissen@ieee.org>
2025-09-14 14:33:37 +0100

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,4 +1,24 @@
+resource "aws_security_group" "node_group_remote_access" {
+  name   = "allow HTTP"
+  vpc_id = module.vpc.vpc_id
+  ingress {
+    description = "port 22 allow"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = " allow all outgoing traffic "
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 module "eks" {
+
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
@@ -7,54 +27,104 @@ module "eks" {
   cluster_endpoint_public_access  = false
   cluster_endpoint_private_access = true
 
-  cluster_security_group_additional_rules = {
-    allow_bastion_https = {
-      cidr_blocks = [aws_security_group.bastion_sg.id]  # limiter au bastion SG
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      type        = "ingress"
-      description = "Allow HTTPS from Bastion"
-    }
-  }
-
-  cluster_addons = {
-    coredns   = { most_recent = true }
-    kube-proxy = { most_recent = true }
-    vpc-cni    = { most_recent = true }
-  }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets     # Workers dans private subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    ng1 = {
-      desired_size  = 1
-      max_size      = 3
-      min_size      = 1
-      instance_types = ["t3.large"]
-
-      remote_access = {
-        ec2_ssh_key               = aws_key_pair.bastion_key.key_name
-        source_security_group_ids = [aws_security_group.bastion_sg.id]
-      }
-
-      disk_size = 35
-    }
-  }
-
+  //access entry for any specific user or role (jenkins controller instance)
   access_entries = {
-    bastion_role_access = {
-      principal_arn = aws_iam_role.bastion_role.arn
+    # One access entry with a policy associated
+    example = {
+      principal_arn = aws_iam_role.bastion_eks_role.arn
+
       policy_associations = {
-        admin = {
-          policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = { type = "cluster" }
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
         }
       }
     }
   }
 
+
+  cluster_security_group_additional_rules = {
+    access_for_bastion_jenkins_hosts = {
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow all HTTPS traffic from jenkins and Bastion host"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      type        = "ingress"
+    }
+  }
+
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  # EKS Managed Node Group(s)
+
+  eks_managed_node_group_defaults = {
+
+    instance_types = ["t3.large"]
+
+    attach_cluster_primary_security_group = true
+
+  }
+
+
+
+  eks_managed_node_groups = {
+
+    tws-demo-ng = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+
+      instance_types = ["c7i-flex.large"]
+      capacity_type  = "SPOT"
+
+      disk_size                  = 35
+      use_custom_launch_template = false # Important to apply disk size!
+
+      remote_access = {
+        ec2_ssh_key               = resource.aws_key_pair.deployer.key_name
+        source_security_group_ids = [aws_security_group.node_group_remote_access.id]
+      }
+
+      tags = {
+        Name        = "tws-demo-ng"
+        Environment = "dev"
+        ExtraTag    = "e-commerce-app"
+      }
+    }
+  }
+
   tags = local.tags
+
+
+}
+
+data "aws_instances" "eks_nodes" {
+  instance_tags = {
+    "eks:cluster-name" = module.eks.cluster_name
+  }
+
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+
+  depends_on = [module.eks]
 }
