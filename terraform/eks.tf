48f52c54cf7aefa47dc38d7958d2b6bb374fae27
wissNasri <nasri.wissen@ieee.org>
2025-09-15 01:33:18 +0100

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -30,8 +30,7 @@ locals {
     "185.199.108.0/22",
     "140.82.112.0/20",
     "143.55.64.0/20",
-    "2a0a:a440::/29",
-    "2605:d000:1::/48"
+
     # ASSUREZ-VOUS DE COPIER LA LISTE COMPLÈTE DE LA COMMANDE CURL
   ]
 
