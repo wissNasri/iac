d708c9ef0f5108755e080f9c1be3de9288278524
wissNasri <nasri.wissen@ieee.org>
2025-09-15 17:30:32 +0100

    Create github-runners.tf

---
+++ terraform/github-runners.tf
@@ -0,0 +1,49 @@
+# Fichier: iac/terraform/github-runners.tf
+# Description: Provisionne des runners GitHub auto-hébergés dans les sous-réseaux privés.
+
+# 1. Récupérer le token GitHub depuis AWS Secrets Manager
+data "aws_secretsmanager_secret_version" "github_pat" {
+  # Le nom que vous avez donné au secret dans la console AWS
+  secret_id = "self_hosted_runner_pat" 
+}
+
+# 2. Créer un groupe de sécurité pour les runners
+resource "aws_security_group" "self_hosted_runner_sg" {
+  name        = "self-hosted-runner-sg"
+  description = "Security group for self-hosted GitHub runners"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG Self-Hosted Runner"
+  }
+}
+
+# 3. Utiliser le module Terraform pour créer les runners
+module "github_runners" {
+  source  = "philips-labs/github-runner/aws"
+  version = "2.2.1"
+
+  github_owner = "WissNasri"
+  github_token = data.aws_secretsmanager_secret_version.github_pat.secret_string
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  instance_type = "t3.medium"
+  iam_role_name = "GitHubRunnerInstanceRole"
+  
+  security_group_ids = [aws_security_group.self_hosted_runner_sg.id]
+
+  runner_labels = ["self-hosted", "aws-private-runner"]
+  
+  tags = {
+    Name = "GitHub-Self-Hosted-Runner"
+  }
+}
