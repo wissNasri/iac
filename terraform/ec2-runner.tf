2150f6ab0c7c91dc150642b1b402af6a48b1057b
wissNasri <nasri.wissen@ieee.org>
2025-09-17 22:35:37 +0100

    Update ec2-runner.tf

--- terraform/ec2-runner.tf
+++ terraform/ec2-runner.tf
@@ -8,12 +8,12 @@ resource "aws_security_group" "dedicated_runner_sg" {
   vpc_id      = module.vpc.vpc_id
 
   # Autoriser SSH uniquement depuis ton IP publique
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # ⚠️ remplace par ton IP publique
-  }
+#  ingress {
+ #   from_port   = 22
+  #  to_port     = 22
+   # protocol    = "tcp"
+    #cidr_blocks = ["0.0.0.0/0"] # ⚠️ remplace par ton IP publique
+  #}
 
   # Sortie vers internet (GitHub, apt, etc.)
   egress {
@@ -33,11 +33,11 @@ resource "aws_instance" "dedicated_github_runner" {
   ami           = data.aws_ami.os_image.id 
   instance_type = "t3.micro" # ⚠️ Free Tier. Change si tu veux plus puissant
 
-  # Placement dans un subnet PUBLIC
-  subnet_id = module.vpc.public_subnets[0]
+  # Placement dans un subnet prive
+  subnet_id = module.vpc.private_subnets[0]
 
-  # Attribution d’une IP publique
-  associate_public_ip_address = true
+  # On désactive l'IP publique
+  associate_public_ip_address = false
 
   # Attache le rôle IAM minimaliste
   iam_instance_profile = aws_iam_instance_profile.self_hosted_runner_profile.name
