afd8b05697a4f2f9558a3d0184b0d99f262fedfd
wissNasri <nasri.wissen@ieee.org>
2025-09-17 22:33:49 +0100

    Update bastion_ec2.tf

--- terraform/bastion_ec2.tf
+++ terraform/bastion_ec2.tf
@@ -22,22 +22,22 @@ data "aws_ami" "os_image" {
 
 resource "aws_security_group" "allow_user_bastion" {
   name        = "bastion_host_SG"
-  description = "Allow user to connect"
+  description = "Security Group for the Bastion Host (SSM access only)"
   vpc_id      = module.vpc.vpc_id
-  dynamic "ingress" {
-    for_each = [
-      { description = "port 22 allow", from = 22, to = 22, protocol = "tcp", cidr = ["0.0.0.0/0"] },
-      { description = "port 80 allow", from = 80, to = 80, protocol = "tcp", cidr = ["0.0.0.0/0"] },
-      { description = "port 443 allow", from = 443, to = 443, protocol = "tcp", cidr = ["0.0.0.0/0"] }
-    ]
-    content {
-      description = ingress.value.description
-      from_port   = ingress.value.from
-      to_port     = ingress.value.to
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr
-    }
-  }
+ # dynamic "ingress" {
+  #  for_each = [
+   #   { description = "port 22 allow", from = 22, to = 22, protocol = "tcp", cidr = ["0.0.0.0/0"] },
+    #  { description = "port 80 allow", from = 80, to = 80, protocol = "tcp", cidr = ["0.0.0.0/0"] },
+     # { description = "port 443 allow", from = 443, to = 443, protocol = "tcp", cidr = ["0.0.0.0/0"] }
+    #]
+   # content {
+    #  description = ingress.value.description
+     # from_port   = ingress.value.from
+      #to_port     = ingress.value.to
+      #protocol    = ingress.value.protocol
+      #cidr_blocks = ingress.value.cidr
+    #}
+  #}
 
   egress {
     description = " allow all outgoing traffic "
@@ -57,10 +57,12 @@ resource "aws_instance" "bastion_host" {
   instance_type          = var.instance_type
  # key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.allow_user_bastion.id]
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc.private_subnets[0]
   iam_instance_profile   = aws_iam_instance_profile.bastion_profile.name  
   user_data              = file("${path.module}/bastion_user_data.sh")
-   
+  # On désactive l'IP publique
+  associate_public_ip_address = false
+
   tags = {
     Name = "Bastion-Host"
   }
