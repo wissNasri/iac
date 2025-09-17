e7944c91b75cb38ce5f0bc88d153b0a0548549f9
wissNasri <nasri.wissen@ieee.org>
2025-09-17 22:28:07 +0100

    Update iam-policy.tf

--- terraform/iam-policy.tf
+++ terraform/iam-policy.tf
@@ -1,4 +1,13 @@
+# Fichier : terraform/iam-policy.tf
+
 resource "aws_iam_role_policy_attachment" "bastion_eks_access" {
   role       = aws_iam_role.bastion_eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# --- AJOUT NÉCESSAIRE POUR L'ACCÈS SÉCURISÉ ---
+# Attache la politique pour AWS Systems Manager Session Manager
+resource "aws_iam_role_policy_attachment" "bastion_ssm_access" {
+  role       = aws_iam_role.bastion_eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
