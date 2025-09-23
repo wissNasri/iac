9770368483785ad4f5900817467d529efca67531
wissNasri <nasri.wissen@ieee.org>
2025-09-23 01:03:51 +0100

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -107,8 +107,8 @@ module "eks" {
       desired_size = 3
 
       instance_types = ["m7i-flex.large"]
-      capacity_type  = "ON_DEMAND"
-      #capacity_type  = "SPOT"
+      #capacity_type  = "ON_DEMAND"
+      capacity_type  = "SPOT"
       disk_size                  = 35
       use_custom_launch_template = false # Important to apply disk size!
 
