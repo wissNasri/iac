672bc674c5817515cdd55c1d695e8dc8cb3c451d
wissNasri <nasri.wissen@ieee.org>
2025-09-25 05:33:35 +0100

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,11 +3,10 @@
 terraform {
   # Votre backend S3 existant
   backend "s3" {
-    bucket         = "terraform-s3-backend-tws-hackathon111"
+    bucket         = "terraform-s3-backend-tws-hackathon1111"
     key            = "backend-locking"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+
   }
 
 
