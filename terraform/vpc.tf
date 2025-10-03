34134a5ceafa2673ce7d5cf48b2a5fdcfe78d00d
wissNasri <nasri.wissen@ieee.org>
2025-10-03 12:38:45 +0100

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,7 +9,6 @@ module "vpc" {
   public_subnets  = local.public_subnets
   private_subnets = local.private_subnets
 
-
   enable_nat_gateway     = true
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
@@ -18,12 +17,8 @@ module "vpc" {
     "kubernetes.io/role/elb" = 1
   }
 
-
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
-  # Ensure public subnets auto-assign public IPs
   map_public_ip_on_launch = true
-
-
 }
