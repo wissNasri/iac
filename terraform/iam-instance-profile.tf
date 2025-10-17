77f9a49346f4e55861d688e9647b790e7374a24c
wissNasri <nasri.wissen@ieee.org>
2025-10-18 00:17:06 +0100

    Update iam-instance-profile.tf

--- terraform/iam-instance-profile.tf
+++ terraform/iam-instance-profile.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_instance_profile" "bastion_profile" {
-  name_prefix = "bastion-profile-"
+  name_prefix = "bastion-profile-testFINALE"
   role = aws_iam_role.bastion_eks_role.name
   depends_on = [
     aws_iam_role.bastion_eks_role
